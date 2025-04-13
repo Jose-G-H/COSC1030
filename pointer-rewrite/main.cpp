262066bb34a9b1c0e69cf8d2da1e406f4c0d96a9
Jose Garcia <josegarcia@westernwyoming.edu>
2025-04-13 19:21:33 +0000

    Ready for grading.

--- pointer-rewrite/main.cpp
+++ pointer-rewrite/main.cpp
@@ -1,7 +1,38 @@
 //
-// Name
-// Date
+// Jose Garcia
+// 04/13/25
 // Pointer Rewrite Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+//function prototype
+int doSomething(int *, int *);
+
+int main()
+{
+    int x = 10, y = 2;
+
+    cout << "Before calling the function:\n"
+         << "x = " << x << endl
+         << "y = " << y << endl << endl;
+
+    cout << "The function returns "
+         << doSomething(&x, &y) << endl;
+
+    cout << "\nAfter calling the function:\n"
+         << "x = " << x << endl
+         << "y = " << y << endl;
+    
+    return 0;
+}
+
+int doSomething(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y * 10;
+    *y = temp * 10;
+    return *x + *y;
+}
