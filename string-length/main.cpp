59d84326e2e10138796e1327f0ed5e5a75c9a284
Jose Garcia <josegarcia@westernwyoming.edu>
2025-04-28 04:15:12 +0000

    Ready for grading.

--- string-length/main.cpp
+++ string-length/main.cpp
@@ -1,7 +1,39 @@
 //
-// Name
-// Date
+// Jose Garcia
+// 04/27/25
 // String Length Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+// Function prototype 
+int count_charac(const char *str);
+
+int main()
+{
+    const int SIZE = 100;
+    char input[SIZE];
+
+    cout << "Enter a string: ";
+    cin.getline(input, SIZE);
+
+    int length = count_charac(input);
+
+    cout << "\nThe string has " << length << " characters." << endl;
+
+    return 0;
+}
+
+// Function to count characters in c string 
+int count_charac(const char *str)
+{
+    int count = 0;
+    while (*str != '\0')
+    {
+        count++;
+        str++;
+    }
+    return count;
+}
